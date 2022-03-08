e6fa15054ed7f92268bc9b072d77d11df3ddfb07
buraktitiz <titizburak10@gmail.com>
2022-03-08 03:03:07 +0300

    Javaspcipt Giriş

---
+++ Javascript/js/01-hello-world.js
@@ -0,0 +1,10 @@
+/* *********    Merhaba Dünya Örnekleri********* */
+
+/* Dökümana (document) yazdırma işlemi ile Merhaba Dünya */
+// document.write('Merhaba Dünya');
+
+/* Uyarı Mesajı (Alert) ile Merhaba Dünya */
+// alert("Merhaba Dünya")
+
+/* console.log ile Merhaba Dünya */
+console.log("Merhaba Dünya")
