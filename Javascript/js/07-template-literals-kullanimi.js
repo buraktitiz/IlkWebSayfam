3ee539544fec0d0551d44c72c8eaec1c0057101d
buraktitiz <titizburak10@gmail.com>
2022-03-11 17:40:13 +0300

    Template Literals çalışması gerçekleştirildi

---
+++ Javascript/js/07-template-literals-kullanimi.js
@@ -0,0 +1,19 @@
+/* ********* Template Literals Kullanımı *********** */
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+let username="`burak`"
+const DOMAIN = "kodluyoruz.org"
+
+let email=username+"@"+DOMAIN
+console.log("Merhaba",username,"Sitemize Hoşgeldin.","Mail adresin: ",email)
+
+let info=`Merhaba ${username} sitemize hoşgeldin. 
+Mail adresin: ${email}
+mail adresinin uzunluuğu: ${email.length}
+Borcunuz: ${(2+5)*10}₺
+Günün saat bilgisi: ${new Date().getHours()}
+
+`
+/* backticks oluşturmak için: Altgr + , */
+
+console.log(info)
