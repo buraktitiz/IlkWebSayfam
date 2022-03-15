11b45414751987b69ccdef3128c9dd5c07f368cf
buraktitiz <titizburak10@gmail.com>
2022-03-15 21:56:32 +0300

    Promp ile kullanıcıdan bilgi alma işlemleri

--- Javascript/js/11-promp-ile-kullanicidan-bilgi-almak.js
+++ Javascript/js/11-promp-ile-kullanicidan-bilgi-almak.js
@@ -1,8 +1,21 @@
 /* *********** prompt ile Kullanıcıdan Bilgi Almak */
 /* https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt */
 
-let fullName=prompt("Lütfen Adınızı Giriniz: ")
+/* let fullName=prompt("Lütfen Adınızı Giriniz: ")
 
 let greeting=document.querySelector("#greeting")
 
-greeting.innerHTML=`${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+greeting.innerHTML=`${greeting.innerHTML} <small style="color:red">${fullName}</small>` */
+
+let bookName=prompt(`kitap adı ne`)
+let bookWriter=prompt(`kitap yazarı kim`)
+let bookType=prompt(`Kitap türü nedir`)
+
+let kitapAdi=document.querySelector(`#kitapAdi`)
+let kitapYazari=document.querySelector(`#kitapYazari`)
+let kitapTuru=document.querySelector(`#kitapTuru`)
+
+kitapAdi.innerHTML=`${bookName}`
+kitapYazari.innerHTML=`${bookWriter}`
+kitapTuru.innerHTML=`${bookType}`
+
