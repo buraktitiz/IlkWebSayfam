b0b92b4f2571d0b206484c0387bbf4cc109e7e7e
buraktitiz <titizburak10@gmail.com>
2022-03-17 17:05:56 +0300

    Karşılaştırma ve mantıksal operatörleri

---
+++ Javascript/js/14-karsilastirma-operatorleri-ve-mantiksal-operatorler.js
@@ -0,0 +1,39 @@
+/* Karşılaştırma Operatörleri ve Mantıksal Operatörler */
+/* https://www.w3schools.com/js/js_comparisons.asp */
+
+let price = "100";
+let user = "burak"
+
+
+/* == Eşitse */
+console.log("==: ", price == 1)
+console.log("==: ", price == 100)
+
+
+/* === Hem değeri hem de türü eşitse */
+console.log("===: ", price === 1)
+console.log("===: ", price === "100")
+
+/* != Eşit değilse */
+console.log(price != "guest")
+
+/* < Küçükse */
+console.log("price<100: ", price < 100)
+
+/* > Büyükse */
+console.log("price>200: ", price > 100)
+
+/* >= Büyük veya eşitse */
+console.log("price>=100: ", price >= 100)
+
+/* && ve  */
+price = 0;
+console.log(price > 0 && user != "guest")
+
+/* || veya */
+console.log(price > 0 || user != "guest")
+
+/* ! değil (tersi) */
+user = "guest"
+price = 1
+console.log(price > 0 && !user == "guest")
