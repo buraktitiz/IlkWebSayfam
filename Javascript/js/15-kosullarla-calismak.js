3a8d59aa33fec99413dc6101c96b2144363384ae
buraktitiz <titizburak10@gmail.com>
2022-03-17 23:54:21 +0300

    Koşullarla çalışma tamamlandı.

--- Javascript/js/15-kosullarla-calismak.js
+++ Javascript/js/15-kosullarla-calismak.js
@@ -1,14 +1,52 @@
 /* *********** Koşullarla Çalışmak ********** */
 
-let username=prompt("Kullanıcı Adınızı Giriniz: ")
+/* let username=prompt("Kullanıcı Adınızı Giriniz: ") */
 
 /* eğer kullanıcı bilgisi varsa ekrana ismini yazdır */
-/* eger(username.length> {console.log(username)}) değilse { console.log("bilgi yok")} */ 
+/* eger(username.length> {console.log(username)}) değilse { console.log("bilgi yok")} */
 /* if(username.length> {console.log(username)}) else { console.log("bilgi yok")} */
-if(username)
+/* if(username)
 {
     console.log(`Kullanıcı Bilginiz: ", ${username}`)
 }
 else {
     console.log("bilgi yok")
+} */
+
+
+ /* //İki sayıdan büyük olanı bulalım 
+
+var x=5;
+var y=6;
+
+if (x > y) {
+    console.log(x + " sayisi " + y + " sayısından küçüktür.");
+} else {
+    console.log(y + " sayisi " + " " + x + " sayısından büyüktür.");
 }
+ */
+
+
+/* // Vize - Final Not ortalamasını Kullanıcıdan alarak Bulma 
+
+let vize=prompt("Vize Notunuzu Girin: ");
+let final =prompt("Final notunuzu Girin: ");
+
+let ortalama=((vize*0.4)+(final*0.6));
+
+if(ortalama<31)
+{
+    console.log(`Kaldınız,${ortalama}`)
+}
+else if(ortalama>30 &&ortalama<50)
+{
+    console.log(`Koşullu Geçtiniz,${ortalama}`)
+}
+else if(ortalama>50 &&ortalama<85)
+{
+    console.log(`Geçtiniz,${ortalama}`)
+}
+else if(ortalama>84 && ortalama<=100)
+{
+    console.log(`En yüksek ortalama,${ortalama}`)
+} */
