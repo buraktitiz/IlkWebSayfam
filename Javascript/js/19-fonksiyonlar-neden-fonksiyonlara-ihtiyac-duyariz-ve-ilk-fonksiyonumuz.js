3d262574e9c421728e498dec5ce5b2ab5fe32c31
buraktitiz <titizburak10@gmail.com>
2022-03-22 02:14:31 +0300

    Fonksiyon nedir? Neden kullanırız?

--- Javascript/js/19-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
+++ Javascript/js/19-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
@@ -14,7 +14,7 @@ hello() // Çalıştırmak için hello'yu () ile tanımlayarak çalıştırıyor
 // Ayrıca yukarıda hellowworld'ü hello'nun içine tanımladık. Orada tanımlı olduğundan dolayı hello çağrıldığında hem Merhaba hem de Hello World yazacaktır.
 
 
-
+/* 
 
 function topla(sayi1, sayi2) {
     return sayi1 + sayi2;
@@ -29,12 +29,18 @@ sayi1 * sayi2; // return tanımlı değil
 result=carp(10,20); // undefined 
 console.log(topla(10,40)) // 50
 
+ */
 
+
+
+
+/*
 var ekle = function topla(sayi1, sayi2) {
     return sayi1 + sayi2;
 };
 
 /* var sonuc1 = ekle(10,20); 
-var sonuc2 = topla(10,20); // geçersiz */
+var sonuc2 = topla(10,20); // geçersiz 
 console.log(ekle(10,20)) // 30 
 console.log(topla(10,80)) // 90
+  */
