3f9664aa24b259e310d065b3c2177f12ef7a60eb
buraktitiz <titizburak10@gmail.com>
2022-03-22 22:29:02 +0300

    Fonksiyon nedir? Nasıl kullanılır?

--- Javascript/js/19-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
+++ Javascript/js/19-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
@@ -1,11 +1,11 @@
 /*  ************ Fonksiyonlar Nedir? Neden ihtiyaç duyarız? */
 /* İlk Fonksiyonumuzu Tanımlamak: */
 
-function helloWorld(){
+function helloWorld() {
     console.log("Hello World")
 }
 
-function hello(){
+function hello() {
     console.log("Merhaba")
     helloWorld()
 }
@@ -55,15 +55,64 @@ function mesajVer() {
 
 
 
- /* 
-    const carpim = (sayi1, sayi2) => sayi1 * sayi2;
+/* 
+   const carpim = (sayi1, sayi2) => sayi1 * sayi2;
 
-    // Daha uzun hali ise;
-   // const carpim = function(sayi1,sayi2){
-    // return sayi1 * sayi2;
-   // } 
-    
-    console.log(carpim(3, 5)); 
-  */
+   // Daha uzun hali ise;
+  // const carpim = function(sayi1,sayi2){
+   // return sayi1 * sayi2;
+  // } 
+   
+   console.log(carpim(3, 5)); 
+ */
 
-    
+
+
+/* 
+    // bir fonksiyon yazalım bu fonksiyon sayının kuvvetini alsın.
+// pow(2,2) = 4
+// pow(2,3) = 8
+// pow(2,4) = 16 değerlerini verecek şekilde diyelim.
+
+// 1.ci yol (recursion patterni ile düşünmeden)
+/* function pow(x, n) {
+    let result = 1;
+  
+    for (let i = 0; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  console.log(pow(3,4)) */
+
+
+
+/*   
+alert(pow(2, 3)); // 8
+// recursion ile
+function pow(x, n) {
+  if (n == 1) {
+    return x;
+  } else {
+    return x * pow(x, n - 1);
+  }
+}
+
+alert(pow(2, 3)); // 8 */
+
+
+
+/* 
+//Aşağıdaki soruyu recursion ile arrow function ve if else clause kullanarak yeniden nasıl yazarız
+function pow(x, n) {
+    return n == 1 ? x : x * pow(x, n - 1);
+  }
+  console.log(pow(3,4)) */
+
+/* faktöriyel yapalım
+function factoriel(x) {
+    return x == 1 ? x : x * factoriel(x-1);
+}
+console.log(factoriel(6)) //720
+ */
