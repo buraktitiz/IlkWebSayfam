1402e1bf7c6f4d4b46b7e5e794aa75d0e52cb790
buraktitiz <titizburak10@gmail.com>
2022-03-23 22:47:20 +0300

    Fonksiyona parametre atama işlemleri

--- Javascript/js/20-fonksiyonlara-parametre-atamak-ve-fonksiyondan-geridonus-almak.js
+++ Javascript/js/20-fonksiyonlara-parametre-atamak-ve-fonksiyondan-geridonus-almak.js
@@ -8,18 +8,34 @@
 /* 3: Mümkünse fonksiyonun bağımlılıklarını azaltmak gerekir. */
 
 
-let firsName="Lorem"
+let firsName = "Lorem"
 
-function greetings(firsName="", lastName=""){ // bu fonksiyonumuzun özelliği: deafult parametre alıyor, yani şöyle: ("")
+function greetings(firsName = "", lastName = "") { // bu fonksiyonumuzun özelliği: deafult parametre alıyor, yani şöyle: ("")
     // Ama default parametre de vermeyebilirdik. Yani (firsName, lastName)
-
     //console.log(`Merhaba ${firsName ? firsName:""}`)
-    console.log(`Merhaba ${firsName}`)
+    //console.log(`Merhaba ${firsName}`)
     console.log(`Merhaba ${firsName} ${lastName}`)
 }
 
 
 console.log(firsName) // değişken bilgisi var
 greetings() // fonksiyona parametre göndermedik
-greetings("Parametre")  // fonksiyona parametre gönderdiğimiz hali
+greetings("Parametre") // fonksiyona parametre gönderdiğimiz hali
 
+function greetings2(firsName, lastName) {
+    let info=`Merhabaaaa ${firsName} ${lastName}`
+    return info
+}
+
+let greetingsInfo=greetings2("Ad","Soyad")
+//let info= "deneme"
+console.log(greetingsInfo)
+
+
+function domIdWriteInfo(id,info){
+    let domObject=document.querySelector(`#${id}`)
+    domObject.innerHTML=info
+}
+let htmlInfo=`<span style="color:red">Renk Kızıllaştı</span>`
+domIdWriteInfo('greeting',htmlInfo)
+domIdWriteInfo('info',greetings2("Lorem","Ipsum"))
