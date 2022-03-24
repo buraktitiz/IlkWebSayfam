bffca97ae2857365ccf090323227d924f887f4be
buraktitiz <titizburak10@gmail.com>
2022-03-24 14:10:47 +0300

    Arrow Fonksiyon kullanımı

--- Javascript/js/21-arrow-function-kullanimi.js
+++ Javascript/js/21-arrow-function-kullanimi.js
@@ -5,7 +5,9 @@ function hello(firsName) {
 }
 hello("JavaScript")
 
-const helloFuncV1 = (firsName) =>{console.log(`Merhaba ${firsName}`)}
+const helloFuncV1 = (firsName) => {
+    console.log(`Merhaba ${firsName}`)
+}
 // 1. function diye yazdığımızı const diye tanımlıyoruz. Aslında bir değişken tanımlama gibi çalışıyor
 // 2. ='den sonra içine girdiğimiz olan parametreyi belirttik (firstName). Parametreyi belirttikten sonra path arrow(=>) ile bir fonksiyon işlemi olduğunu belirtip return'ü de süslü parantez içine yazdık.
 helloFuncV1("helloFuncV1")
@@ -13,18 +15,18 @@ helloFuncV1("helloFuncV1")
 
 
 
-const helloFuncV2=firsName=>console.log(`Merhaba ${firsName}`)
+const helloFuncV2 = firsName => console.log(`Merhaba ${firsName}`) // bir parametre bir dönüş işlemi 
 helloFuncV2("helloFuncV2")
 
 
 
-const helloFuncV3=(firsName,lastName)=>console.log(`Merhaba ${firsName} ${lastName}`)
-helloFuncV3("helloFuncV3", "Last Name info")
+const helloFuncV3 = (firsName, lastName) => console.log(`Merhaba ${firsName} ${lastName}`)
+helloFuncV3("helloFuncV3", "Last Name info") // birden fazla değişken var ama tek bir tane işlem olduğu için süslü parantez kullanmaya gerek duymuyoruz
 
 
 
-const helloFuncV4=(firsName,lastName)=>{
-    let info=`Merhaba ${firsName} ${lastName}`
+const helloFuncV4 = (firsName, lastName) => { // birden fazla değişken olduğu için süslü parantezleri kullanıyoruz.
+    let info = `Merhaba ${firsName} ${lastName}`
     console.log(info)
     return info
 }
