bc6d643951cef5a161c343764d465e6b5d5342c2
buraktitiz <titizburak10@gmail.com>
2022-03-28 15:33:20 +0300

    Local Storage Bölüm Sonu Egzersizi

--- Javascript/js/26-locaStorage-bolum-sonu-egzersizi.js
+++ Javascript/js/26-locaStorage-bolum-sonu-egzersizi.js
@@ -1,6 +1,6 @@
 /* Local Storage Bölüm Sonu Egzersizi */
 
-let counter = 0
+let counter = localStorage.getItem('counter') ? Number(localStorage.getItem('counter')) : 0
 let counterDOM = document.querySelector('#counter')
 let increaseDOM = document.querySelector('#increase')
 let decreaseDOM = document.querySelector('#decrease')
@@ -8,17 +8,18 @@ let decreaseDOM = document.querySelector('#decrease')
 counterDOM.innerHTML = counter
 
 increaseDOM.addEventListener("click", clickEvent)
-
+/* 
 function clickEvent() {
     console.log(this.id)
 }
-
+ */
 decreaseDOM.addEventListener("click", clickEvent)
 
 function clickEvent() {
-    console.log(this.id)
-    counterDOM.innerHTML=counter
-    this.id == "increase" ? counter = counter += 1 : counter = counter -= 1 // increase'e bak. Eğer increase işlemi geliyorsa counter'ı 1 artır, yoksa 1 azalt.
-    localStorage.setItem('counter',counter)
-    counterDOM.innerHTML=counter
+    /*  console.log(typeof(counter))
+     console.log(this.id) */
+    /* counterDOM.innerHTML=counter */
+    this.id == "increase" ? counter += 1 : counter -= 1
+    localStorage.setItem('counter', counter)
+    counterDOM.innerHTML = counter
 }
