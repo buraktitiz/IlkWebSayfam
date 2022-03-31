01f3fbac8a61d91e4d97951e876ee940a29eda0a
buraktitiz <titizburak10@gmail.com>
2022-03-31 16:47:56 +0300

    Array

---
+++ Javascript/js/30-dizi-olusturmak-ve-dizi-icindeki-elemana-ulasmak.js
@@ -0,0 +1,11 @@
+/* array - dizilerle çalışmak */
+
+/* Array oluşturmak */
+
+/* Array içindeki eleman sayısını öğrenmek */
+
+/* Array içindeki ilk elemanın çağırılması */
+
+/* Array içindeki son elemanın çağırılması */
+
+/* değişken içindeki bilginin Array olup olmadığının kontrol edilmesi */
