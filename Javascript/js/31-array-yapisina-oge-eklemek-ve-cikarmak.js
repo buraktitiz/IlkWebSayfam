7137ad698fa147b8cf46e087bd02696c551ad2a9
buraktitiz <titizburak10@gmail.com>
2022-04-07 01:34:41 +0300

    Array yapısına öge eklemek ve çıkarmak

--- Javascript/js/31-array-yapisina-oge-eklemek-ve-cikarmak.js
+++ Javascript/js/31-array-yapisina-oge-eklemek-ve-cikarmak.js
@@ -23,5 +23,20 @@ console.log("lastName: ",lastName,"items: ",items);
 let firstName=items.shift() // ilk elemanı firstName içine ekledik
 console.log("firstName: ",firstName,"items: ",items);
 
+//**Array içindeki bir ögenin bilgisinin değiştirilmesi
+//**ilk ögenin değiştirilmesi
+items[0]=5; // ilk elemanı 10'du 5 yaptık
+console.log(items) 
 
+//**son ögenin değiştirilmesi
+items[items.length-1]=100 //son elemanı 40'tı. 40'ı silip 100'le değiştirdik
+console.log(items)
+
+
+//** Tanımlamaları doğru yapmamız çok önemlidir. Örneğin;
+items[2]=1000
+console.log(items) // yazarsak 2. index'ine 1000 ögesini eklemiş olacağız. Bu kullanımda farkedilmeyebilir fakat 
+
+items[10000]=789
+console.log(items) // yaparsak on bininci(10000) ögenin değeri 789 olacaktır. Bu yüzden tanımlamalar önemlidir.
 
