8914a2b2e0dfb18b83debd39d66bf9a697ee71c4
buraktitiz <titizburak10@gmail.com>
2022-04-10 01:56:56 +0300

    For döngüsü kullanımı

--- Javascript/js/33-for-dongusu-kullanimi.js
+++ Javascript/js/33-for-dongusu-kullanimi.js
@@ -4,7 +4,7 @@
 // yapılacak işlem
 // }
 
-let users = ["Lorem", "Ipsum", "Dolor"]
+//let users = ["Lorem", "Ipsum", "Dolor"]
 
 // for (let index = 0; index < 10; index++) {
 //     console.log(index)
@@ -23,4 +23,33 @@ let users = ["Lorem", "Ipsum", "Dolor"]
 //     const liDOM=document.createElement('li');
 //     liDOM.innerHTML = users[index]
 //     userListDOM.appendChild(liDOM)
+// }
+
+
+
+
+// for (let index = 10; index >=0; index--) {
+//     console.log(index);
+// }
+
+
+// for (let index = 20; index <=40; index++) {
+//     console.log(index);
+// }
+
+
+
+// for (let index = 0; index <=50; index++) {
+//     if (index%2==0) {
+//         console.log(index);
+//     }
+// }
+
+
+
+
+// for (let index = 0; index <=50; index++) {
+//     if (index%2==1) {
+//         console.log(index);
+//     }
 // }
