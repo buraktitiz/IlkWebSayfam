0f8f9ba24d868be818880415180f9da43340e669
buraktitiz <titizburak10@gmail.com>
2022-04-11 04:27:52 +0300

    While döngüsü kullanımı

--- Javascript/js/25-localStorage-icerisinde-farkli-turden-bilgilerin-saklanması.js
+++ Javascript/js/25-localStorage-icerisinde-farkli-turden-bilgilerin-saklanması.js
@@ -1,12 +1,12 @@
 /* Local Storage İşlemleri */
 
-let user={userName: "buraktitiz", isActive:true}
+/* let user={userName: "buraktitiz", isActive:true}
 console.log(user)
 localStorage.setItem('userInfo',JSON.stringify(user))
 
 let userInfo=localStorage.getItem('userInfo') // Bilgiyi al
 userInfo=JSON.parse(userInfo)
-console.log(userInfo)
+console.log(userInfo) */
 
 //Daha komplex bir yapıda getirmek istediğimizde önce string'e çevirmemiz lazım
 //String'e çevirmek için de JSON ile beraber gelen stringify özelliğini kullanıyoruz
@@ -14,7 +14,7 @@ console.log(userInfo)
 
 
 
-localStorage.setItem('number',11)
+/* localStorage.setItem('number',11)
 localStorage.getItem('number')
 
 
@@ -23,4 +23,4 @@ let items=[1,2,3,user]
 localStorage.setItem('newItem',items)
 localStorage.setItem('newItem',JSON.stringify(items))
 localStorage.getItem('newItem')
-JSON.parse(localStorage.getItem('newItem'))
+JSON.parse(localStorage.getItem('newItem')) */
---
+++ Javascript/js/35-while-dongusu-kullanimi.js
@@ -0,0 +1,15 @@
+/* //** While DÖngüsü */
+
+// let counter=0
+
+// while (counter<10) {
+//     console.log(counter)
+//     counter++;
+// }
+
+
+let whileOrnek="";
+while (whileOrnek=="") {
+    whileOrnek = prompt("Kullanıcı Adınızı Giriniz: ")
+    console.log(whileOrnek)
+}
