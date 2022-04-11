0552abed47edc2e0ee51ce24678163071b95521e
buraktitiz <titizburak10@gmail.com>
2022-04-11 14:49:53 +0300

    Array forEach metodu

--- Javascript/js/36-array-foreach-metodu.js
+++ Javascript/js/36-array-foreach-metodu.js
@@ -1,16 +1,38 @@
 /* //** Array forEach Metodu */
 
-const PRODUCTS=["Laptop","Phone","Desktop PC","Speaker","Server","Mouse","Keyboard"]
+const PRODUCTS = ["Laptop", "Phone", "Desktop PC", "Speaker", "Server", "Mouse", "Keyboard"]
 
 // PRODUCTS.forEach((product,index,array) =>array[index]=product+"111")
-PRODUCTS.forEach((product,index,array) =>array[index]=`${product.toUpperCase()}`) // toUpperCase
+PRODUCTS.forEach((product, index, array) => array[index] = `${product.toUpperCase()}`) // toUpperCase
 
 console.log(PRODUCTS)
 
 
-const userListDOM=document.querySelector('#userList');
-PRODUCTS.forEach((item)=>{
-    const liDOM=document.createElement('li');
+const userListDOM = document.querySelector('#userList');
+PRODUCTS.forEach((item) => {
+    const liDOM = document.createElement('li');
     liDOM.innerHTML = item
     userListDOM.append(liDOM)
 })
+
+
+
+// Şimdi de forEach kullanarak yeni bir array oluşturabileceğimiz bir fonksiyon yazalım.
+// const numbers=[4,11,9]
+// const newArray=[]
+
+// numbers.forEach(function(numbers) {
+//     newArray.push(numbers*3)
+// })
+// console.log(newArray)
+
+
+
+
+//Numbers array'ini kullanarak her elemanının iki fazlasına sahip olan başka bir array 
+// const numbers = [12, 24, 36]
+// const newArray = []
+// numbers.forEach(number => {
+//     newArray.push(number + 2)
+// })
+// console.log(newArray)
