ca586a8cf2d3c82854a80a4c0386227982ea70d6
buraktitiz <titizburak10@gmail.com>
2022-04-12 02:18:49 +0300

    Array Filter Kullanımı

--- Javascript/js/37-array-filter-kullanimi.js
+++ Javascript/js/37-array-filter-kullanimi.js
@@ -30,3 +30,24 @@ const USERS = [{
 // const ACTIVE_USERS = USERS.filter(user => user.isActive);
 const ACTIVE_USERS = USERS.filter(user => user.isActive === false);
 console.log(ACTIVE_USERS)
+
+
+
+
+const person = [{
+    name: "Adem",
+    age: 25,
+    languages: ["JavaScript", "CSS"],
+}, {
+    name: "Oğuz",
+    age: 33,
+    languages: ["Java", "HTML"],
+}];
+//Yaşı 30'dan büyük olanı bulam program
+// const BUYUK_YAS = person.filter(item => item.age > 30)
+// console.log(BUYUK_YAS)
+
+
+//JavaScript dilini bileni yazan program
+const JS_BILEN = person.filter(user => user.languages.includes("JavaScript"));
+console.log(JS_BILEN)
