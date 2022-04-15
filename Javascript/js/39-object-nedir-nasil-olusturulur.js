f4def5d5ccdfaadfb5855d7933e66c553fc4e254
buraktitiz <titizburak10@gmail.com>
2022-04-15 16:24:25 +0300

    Object (Nesne) Nedir? Nasıl Oluşturulur

---
+++ Javascript/js/39-object-nedir-nasil-olusturulur.js
@@ -0,0 +1,17 @@
+/* //** Object (Nesne) Nedir? Nasıl Oluşturulur */
+
+let arrayObj=[1,2,3]
+let object1={obf:1}
+
+console.log("arrayObj", typeof(arrayObj))
+console.log("object1", typeof(object1))
+
+let item1=new object()
+let item2={}
+console.log("item1", typeof(item1))
+console.log("item2", typeof(item2))
+
+//Object Oluşturmak
+
+let item3={}
+let item4=new object()
