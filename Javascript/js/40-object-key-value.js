2cf7f78e6ffddaf6f1237302aa528938908aab0d
buraktitiz <titizburak10@gmail.com>
2022-04-16 18:40:31 +0300

    Object - Key - Value

---
+++ Javascript/js/40-object-key-value.js
@@ -0,0 +1,16 @@
+/* //** Object - Key - Value */
+
+//[key1:1, key2:2,key3:3]
+let laptop1={
+    brand:"Apple",
+    model:"MacBook Pro",
+    "2kg":2,
+    modelName:"MacBook Pro",
+    //model-name:"MacBook Pro",
+    model_name:"MacBook Pro",
+}
+console.log(laptop1)
+
+console.log(laptop1.brand, laptop[brand])
+console.log(laptop1.model, laptop[model])
+console.log(laptop1["2kg"])
