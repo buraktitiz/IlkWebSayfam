22c81b9f6c4f71c11981019b1626797b3a023fe8
buraktitiz <titizburak10@gmail.com>
2022-04-26 02:49:05 +0300

    Fetch API İle Çalışmak

--- Javascript/js/44-fetch-api-ile-calismak.js
+++ Javascript/js/44-fetch-api-ile-calismak.js
@@ -13,7 +13,7 @@ fetch("data/settings.json").then(
 
 
 let userListDOM = document.querySelector("#userList");
-// API üzerinden verileri çektik
+// API üzerinden verileri çektik ve sayfada gösterdik
 fetch("https://jsonplaceholder.typicode.com/posts").then(
     response => response.json()
 ).then(responseJson => {
@@ -23,3 +23,33 @@ fetch("https://jsonplaceholder.typicode.com/posts").then(
         userListDOM.appendChild(liDOM)
     })
 })
+
+
+// API üzerinden çekilen veriyi çektik ve konsola yazdırdık
+// GET isteği yapacağız.
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json()) //parse json data
+    .then(function (todos) {
+        todos.forEach((todo) => {
+            console.log(todo.title); //Başlıkları console' a yazdırma
+        });
+    });
+
+
+
+// ** Uygulama1
+// API dokümanında Resources bölümünde belirtilen kaynaklardan /users uzantısındaki verileri alıp console’a yazdıracak kodu yazın.
+fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => response.json()).then(
+    (json) => console.log(json)).catch(
+    (err) => console.log(err)
+)
+
+
+// ** Uygulama2
+// Json Placeholder API servisinin /comments uzantısında mevcut verilerden, her bir comment içerisindeki email’leri console’a yazdıracak kodu yazın.
+fetch("https://jsonplaceholder.typicode.com/comments").then(
+    (response) => response.json()).then(
+    (json) => json.forEach((item) => console.log(item.email))).catch(
+    (err) => console.log(err)
+)
