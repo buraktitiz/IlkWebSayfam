0cc8ead887d6addd683d3794224b922ef503be4b
buraktitiz <titizburak10@gmail.com>
2022-04-26 02:09:12 +0300

    fetch api ile çalışmak

---
+++ Javascript/js/44-fetch-api-ile-calismak.js
@@ -0,0 +1,25 @@
+/* //** fetch api ile çalışmak */
+
+
+// JSON dosyadan veri çektik
+fetch("data/settings.json").then(
+    response => {
+        return response.json();
+    }
+).then(responseJson => {
+    console.log(responseJson)
+    console.log(responseJson.userName)
+})
+
+
+let userListDOM = document.querySelector("#userList");
+// API üzerinden verileri çektik
+fetch("https://jsonplaceholder.typicode.com/posts").then(
+    response => response.json()
+).then(responseJson => {
+    responseJson.forEach(item => {
+        let liDOM = document.createElement('li')
+        liDOM.innerHTML = item.title
+        userListDOM.appendChild(liDOM)
+    })
+})
