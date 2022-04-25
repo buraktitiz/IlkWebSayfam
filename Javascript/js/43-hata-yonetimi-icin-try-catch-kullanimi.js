56f3aba0e27b15765331eb641b4fd121fbcd7a44
buraktitiz <titizburak10@gmail.com>
2022-04-26 01:00:56 +0300

    Hata Yönetimi İçin try-catch Kullanımı

---
+++ Javascript/js/43-hata-yonetimi-icin-try-catch-kullanimi.js
@@ -0,0 +1,14 @@
+/* //** Hata Yönetimi İçin try-catch Kullanımı */
+
+let products;
+
+try {
+    products.forEach(item => console.log(item));
+} catch (error){
+    console.log("error", error);
+}
+console.log("hata yönetimi düzgün çalışıyor")
+
+let info="kodluyoruz"
+console.log(info)
+
