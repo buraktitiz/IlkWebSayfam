e1e07421acd47fc48d4c0660af1004805f221e9e
buraktitiz <titizburak10@gmail.com>
2022-04-26 01:08:20 +0300

    Hata Yönetimi İçin try-catch Kullanımı

--- Javascript/js/43-hata-yonetimi-icin-try-catch-kullanimi.js
+++ Javascript/js/43-hata-yonetimi-icin-try-catch-kullanimi.js
@@ -1,6 +1,58 @@
 /* //** Hata Yönetimi İçin try-catch Kullanımı */
 
-let products;
+// try...catch ifadesini basitçe ifade edersek; Herhangi bir iş yaparken bir hatayla karşılaşılma durumunun ele alınması anlamına gelmektedir.
+
+// try anahtar kelimesi kodları çalışma zamanında test etmek için kullanılırken, catch anahtar kelimesi ise çalışma zamanında hata çıkması durumunda bu hataları yakalamak için kullanılır.
+
+// Basit bir try catch bloğu şu şekildedir.
+
+/* try {
+    //çalıştırılacak kodlar
+} catch (error) {
+    //hata yakalandığı zaman çalışacak kodlar    
+} finally {
+    //try tamamlandıktan sonra gata olsa a olmasa da çalışacak kodlar 
+}
+ */
+
+// catch bloğunda yazılmış olan hata değişkeni oluşacak istisnayı taşıyacak olan değişken adıdır. Buraya herhangi bir isim verilebilir fakat yaygın olarak exceptionın ilk harfi olan e, err gibi ifadelerle isimlendirilir
+
+/* Throw İle İstisnalar Oluşturma
+
+Normalde kod çalışırken bir hata oluştuğunda, JavaScript durur ve bir hata mesajı oluşturur. Bu duruma JavaScript bir istisna atar (bir hata atar) olarak adlandırılır.
+
+JavaScript aslında iki özelliğe sahip bir Error nesnesi oluşturacaktır: isim ve mesaj.
+
+throw ifadesi bize özel hatalarımızı oluşturmanıza olanak tanır. Teknik olarak throw ile bir istisna (hata) atılabilir.
+
+throw "Çok büyük";    // Çok büyük hatası at, oluştur
+throw 500;            // 500 numarası hatasını at, oluştur
+
+Sonuç olarak throw ile birlikte try...catch ifadesini kullanırsak, program akışını kontrol edebilir ve özel hata mesajları oluşturabiliriz.
+
+Basit bir örnekle daha iyi anlamaya çalışalım.
+
+<input type="text" id="dogumTarihi" />
+<button onclick="valid()">Gönder</button>
+
+Yukarıdaki gibi ‘dogumTarihi’ adında bir input nesnesi oluşturalım ve butona tıkladığımızda eğer ‘dogumTarihi’ adındaki input nesnemize bir şey girilmediyse throw ifadesiyle kolay bir şekilde exception oluşturabiliriz.
+
+function valid(){
+    try {
+        if (document.querySelector("#dogumTarihi").value == "") {
+            throw 'Doğum tarihini boş geçemezsiniz';
+        };
+    }catch(err) {
+        alert(err);
+    }
+}
+ */
+
+
+
+
+
+/* let products;
 
 try {
     products.forEach(item => console.log(item));
@@ -10,5 +62,4 @@ try {
 console.log("hata yönetimi düzgün çalışıyor")
 
 let info="kodluyoruz"
-console.log(info)
-
+console.log(info) */
