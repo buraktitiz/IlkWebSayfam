756ee59ddbc8429aebe3d4035c80c011205e868d
buraktitiz <titizburak10@gmail.com>
2022-09-28 15:57:31 +0300

    Prop Types

--- React/components/hello-react/src/components/User.js
+++ React/components/hello-react/src/components/User.js
@@ -1,13 +1,23 @@
+import PropTypes from "prop-types";
+
 function User({ name, surname, age, isLoggedIn, friends }) {
     return (
         <>
         <h1>{isLoggedIn ? `${name} ${surname} (${age})`:"Giriş yapmadınız"}</h1>
-        
+
         {friends.map((friend) =>(
                 <div key={friend.id}>{friend.name}</div>
             ))}
         </>
     );
+}
+
+User.propTypes={
+    name: PropTypes.string,
+    surname: PropTypes.string,
+    age: PropTypes.number,
+    isLoggedIn: PropTypes.bool,
+    friends: PropTypes.array,
 }
 
 export default User;
