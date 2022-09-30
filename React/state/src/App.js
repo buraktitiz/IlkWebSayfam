7ca6442dfe52dc431b4943eedd554d66555ba64b
buraktitiz <titizburak10@gmail.com>
2022-09-30 22:26:26 +0300

    Array States added

--- React/state/src/App.js
+++ React/state/src/App.js
@@ -3,12 +3,27 @@ import {useState} from 'react';
 function App() {
   const [name, setName]=useState("Burak");
   const [age, setAge]=useState(32);
+  const [friends, setFriends]=useState(["Ahmet","Mehmet"]);
+
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <h2>{age}</h2>
+
       <button onClick={() =>setName("Deagle")}>Click</button>
       <button onClick={() =>setAge("16")}>Click</button>
+
+      <hr /><br />
+
+      <h2>Friends</h2>
+      {friends.map((friend,index) =>(
+      <div key={index}>{friend}</div>
+      ))}
+
+        <br />
+
+        <button onClick={() =>setFriends([...friends,"Ayşe"])}>add new friend</button>
+
     </div>
   );
 }
