3e7048cd5187f2be9ff4a47ae974528d30406112
buraktitiz <titizburak10@gmail.com>
2022-09-30 23:04:05 +0300

    Sayaç Uygulaması geliştirildi

---
+++ React/state/src/components/Counter.js
@@ -0,0 +1,23 @@
+import { useState } from "react" 
+
+function Counter() {
+    const [count,setCount]=useState(0);
+
+    const artir=()=>{
+        setCount(count+1);
+    };
+
+    const azalt=()=>{
+        setCount(count - 1);
+    };
+    return (
+        <div>
+
+        <h1>{count}</h1>
+        <button onClick={azalt}> Azalt</button>
+        <button onClick={artir}> Artır</button>
+    </div>
+);
+}
+
+export default Counter
