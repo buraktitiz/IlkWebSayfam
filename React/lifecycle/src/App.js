a6f2c616f07918fe10446da12874365b96b9debf
buraktitiz <titizburak10@gmail.com>
2022-10-02 18:06:13 +0300

    Added lifecycles with useEffect

---
+++ React/lifecycle/src/App.js
@@ -0,0 +1,33 @@
+import {useState,useEffect} from 'react'
+
+function App() {
+  const[number,setNumber] = useState(0)
+  const[name,setName] = useState("Burak")
+  
+  useEffect(() =>{
+    console.log('Component mount edildi')
+  },[])
+
+useEffect(() =>{
+  console.log('number state güncellendi')
+}, [number])
+
+useEffect(() =>{
+  console.log('name state güncellendi')
+}, [name])
+
+
+return (
+    <div className="App">
+      <h1>{number}</h1>
+      <button onClick={()=>setNumber(number+1)}>Click</button>
+
+      <hr />
+
+      <h1>{name}</h1>
+      <button onClick={()=>setName("Deagle")}>Click</button>
+    </div>
+  )
+}
+
+export default App
