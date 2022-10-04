58ac4167c276a6d85c0b6f3c174e61380afa27f0
buraktitiz <titizburak10@gmail.com>
2022-10-04 20:10:21 +0300

    Stil tanımlarının yapılması

--- React/contacts-app/src/components/Contacts/List/index.js
+++ React/contacts-app/src/components/Contacts/List/index.js
@@ -14,11 +14,18 @@ function List({contacts}) {
     <div>
         <input placeholder='Filter contact' value={filterText} onChange={(e) =>setFilterText(e.target.value)} />
 
-        <ul>
+        <ul className='list'>
             {filtered.map((contact,i)=>(
-            <li key={i}>{contact.fullname}</li>
+            <li key={i}>
+                <span>{contact.fullname}</span>
+                <span>{contact.phone_number}</span>
+            </li>
             ))}
         </ul>
+
+        <p>
+            Total contacts ({filtered.length})
+        </p>
     </div>
     );
 }
--- React/contacts-app/src/components/Contacts/index.js
+++ React/contacts-app/src/components/Contacts/index.js
@@ -1,4 +1,7 @@
 import {useState, useEffect} from 'react'
+
+import './style.css';
+
 import List from './List'
 import Form from './Form'
 
@@ -23,7 +26,8 @@ useEffect(() =>{
 console.log(contacts);
 },[contacts]);
 
-    return <div>
+    return <div id="container">
+        <h1>Contacts</h1>
         <List contacts={contacts} />
         <Form addContact={setContacts} contacts={contacts} />
     </div>
