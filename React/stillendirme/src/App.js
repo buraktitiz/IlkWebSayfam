d258dfb4342906990da4df2caab0e1dc1fe70b35
buraktitiz <titizburak10@gmail.com>
2022-10-22 14:30:17 +0300

    Module CSS

--- React/stillendirme/src/App.js
+++ React/stillendirme/src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
+import A from './components/A'
+import B from './components/B'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div style={{backgroundColor: 'white', color: 'red', paddingTop: '50'}}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita odio deserunt incidunt. Quis commodi esse expedita odio eaque accusamus, obcaecati, numquam officiis at ex quae. Quae praesentium soluta cupiditate eaque!
-        </div>
-        <div class="alert alert-primary" role="alert">
-  A simple secondary alert—check it out!
-</div>
-
+        
+        <A />
+        <B />
 
       </header>
     </div>
---
+++ React/stillendirme/src/components/A/index.js
@@ -0,0 +1,13 @@
+import styles from './styles.module.css';
+
+console.log("A", styles);
+
+function A() {
+  return (
+    <div className={styles.title}>
+        A
+    </div>
+  )
+}
+
+export default A;
