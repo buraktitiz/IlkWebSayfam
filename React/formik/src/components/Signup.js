cfe283a741f922a579698f95eba70099e4157daa
buraktitiz <titizburak10@gmail.com>
2022-10-26 16:21:34 +0300

    Form Validasyon

--- React/formik/src/components/Signup.js
+++ React/formik/src/components/Signup.js
@@ -2,7 +2,7 @@ import { useFormik } from 'formik'
 import validationSchema from './validations'
 
 function Signup() {
-    const { handleChange, handleSubmit, values } = useFormik({
+    const { handleChange, handleSubmit, handleBlur, values, errors, touched } = useFormik({
 
         initialValues: {
     
@@ -18,26 +18,33 @@ function Signup() {
         validationSchema,
         });
     
+        console.log(errors);
     return (
     <div>
         <h1>Sign Up</h1>
             <form onSubmit={handleSubmit}>
 
         <label>Email</label>
-        <input name="email" value={values.email} onChange={handleChange}/>
+        <input name="email" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
         
+        {errors.email && touched.email && <div className="error">{errors.email}</div>}
+
         <br /><br />
         
 
         <label>Password</label>
-        <input name="password" value={values.password} onChange={handleChange}/>
+        <input name="password" value={values.password} onChange={handleChange} onBlur={handleBlur}/>
         
+        {errors.password && touched.password && <div className="error">{errors.password}</div>}
+
         <br /><br />
         
 
         <label>Confirm Password</label>
-        <input name="passwordConfirm" value={values.passwordConfirm} onChange={handleChange}/>
+        <input name="passwordConfirm" value={values.passwordConfirm} onChange={handleChange} onBlur={handleBlur}/>
         
+        {errors.passwordConfirm &&  touched.passwordConfirm &&  <div className="error">{errors.passwordConfirm}</div>}
+
         <br /><br />
         
 
