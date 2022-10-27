326d26602251d975a8e3b82d401d1fa3040afbfa
buraktitiz <titizburak10@gmail.com>
2022-10-27 22:35:50 +0300

    React memo

---
+++ React/memoization/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header({number}) {
+    console.log("Header Component Re-rendered!");
+    return (
+    <div>
+        Header - {number}
+    </div>
+    )
+}
+
+export default React.memo (Header);
