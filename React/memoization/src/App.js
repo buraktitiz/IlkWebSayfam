566bc2085e4e1d80874431703131dd3f6c34dd1e
buraktitiz <titizburak10@gmail.com>
2022-10-28 12:26:49 +0300

    useMemo

--- React/memoization/src/App.js
+++ React/memoization/src/App.js
@@ -1,20 +1,38 @@
 import './App.css';
 
-import {useState} from "react"
+import {useState,useMemo} from "react"
 import Header from './components/Header'
 
 function App() {
 
   const [number,setNumber]= useState(0);
+  const [text,setText]= useState("");
 
+  const data=useMemo(() =>{
+    return calculateObject;
+  },[]);
+
+  /* const data = calculateObject(); */ // Her karakter girdiğinde çalışmaya başlıyor. 
+  //Ve çok fazla değer olduğu zaman sayfanın çok yavaş hareket etmesine ve 
+  //değerlerin çok yavaş girilmesine neden olur
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} />
+      <Header number={number < 5 ? 0 : number} data={data} />
       <hr />
       <h1>{number}</h1>
       <button onClick={()=>setNumber(number+1)}>Click</button>
+      <br /><br />
+      <input calue={text} onChange={({target}) => setText(target.value)} />
     </div>
   );
+}
+
+function calculateObject(){
+  console.log("Calculating...");
+  for(let i=0;i<10;i++){}
+  console.log("Calculating completed...");
+
+  return { name: "Burak"};
 }
 
 export default App;
--- React/memoization/src/components/Header.js
+++ React/memoization/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Header({number}) {
+function Header({number, data}) {
     console.log("Header Component Re-rendered!");
     return (
     <div>
         Header - {number}
+        <br /><br />
+        <code>{JSON.stringify(data)}</code>
+
     </div>
     )
 }
