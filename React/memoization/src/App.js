ef8e28ce1add1ce3122e14609801bed47acc6d43
buraktitiz <titizburak10@gmail.com>
2022-10-29 18:36:42 +0300

    useCallback

--- React/memoization/src/App.js
+++ React/memoization/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {useState,useMemo} from "react"
+import {useState,useMemo, useCallback} from "react"
 import Header from './components/Header'
 
 function App() {
@@ -8,31 +8,26 @@ function App() {
   const [number,setNumber]= useState(0);
   const [text,setText]= useState("");
 
-  const data=useMemo(() =>{
-    return calculateObject;
-  },[]);
+  const increment= useCallback(() => {
+    setNumber((prevState)=>prevState+1) // Bir önceki state'i al. Ve üzerine +1 ekle. 
+    // Bu sayede her butona tıklandığında tekrardan render edilmesinden kurtuluyoruz.
+    // Eğer prevState kullanmayı number+1 olarak yapsaydık aşağıdaki array'e de number eklemeliydik.
+    // Ve yine de header ile render edilmeye devam edecekti. Biz prevState ile bundan kurtulmuş olduk.
+  },[])
 
-  /* const data = calculateObject(); */ // Her karakter girdiğinde çalışmaya başlıyor. 
-  //Ve çok fazla değer olduğu zaman sayfanın çok yavaş hareket etmesine ve 
-  //değerlerin çok yavaş girilmesine neden olur
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} data={data} />
+      <Header increment={increment} />
+
       <hr />
+
       <h1>{number}</h1>
-      <button onClick={()=>setNumber(number+1)}>Click</button>
+      
       <br /><br />
       <input calue={text} onChange={({target}) => setText(target.value)} />
     </div>
   );
 }
 
-function calculateObject(){
-  console.log("Calculating...");
-  for(let i=0;i<10;i++){}
-  console.log("Calculating completed...");
-
-  return { name: "Burak"};
-}
 
 export default App;
