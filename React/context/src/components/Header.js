a7c02b24c1dfd9dbee0b02cabc898bf3fa8a4701
buraktitiz <titizburak10@gmail.com>
2022-10-30 17:02:51 +0300

    Context Provider

--- React/context/src/components/Header.js
+++ React/context/src/components/Header.js
@@ -2,18 +2,20 @@ import {useContext} from 'react'
 
 import ThemeContext from '../context/ThemeContext';
 
-function Button() {
-const {theme,setTheme} = useContext(ThemeContext);
+function Header() {
+    const {theme,setTheme} = useContext(ThemeContext);
 
     return (
     <div>
         Active Theme: {theme}
         <br />
-        <button onClick={()=>setTheme(theme==="light" ? "dark":"light")}>
+        
+        <button onClick={()=>setTheme(theme==="light" ? "dark" : "light")}>
             Change Theme
         </button>
-    </div>
+        </div>
+    
     )
 }
 
-export default Button
+export default Header
