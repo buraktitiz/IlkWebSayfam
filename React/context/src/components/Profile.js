803b856663f766d0f11c53a5be9f061a3a269485
buraktitiz <titizburak10@gmail.com>
2022-11-01 12:13:13 +0300

    Custom Context Hook

--- React/context/src/components/Profile.js
+++ React/context/src/components/Profile.js
@@ -1,10 +1,10 @@
-import {useContext,useState} from 'react'
+import {useState} from 'react'
 
-import UserContext from '../context/UserContext';
+import {useUser} from '../context/UserContext';
 
 function Profile() {
 
-    const {user,setUser}=useContext(UserContext)
+    const {user,setUser}=useUser()
 
     const [loading,setLoading] = useState(false)
     const handleLogin=()=>{
--- React/context/src/context/ThemeContext.js
+++ React/context/src/context/ThemeContext.js
@@ -1,4 +1,4 @@
-import {createContext, useState, useEffect} from 'react';
+import {createContext, useState, useEffect, useContext} from 'react';
 
 const ThemeContext=createContext();
 
@@ -18,4 +18,4 @@ export const ThemeProvider=({children})=>{
     return <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
 }
 
-export default ThemeContext;
+export const useTheme=()=>useContext(ThemeContext)
