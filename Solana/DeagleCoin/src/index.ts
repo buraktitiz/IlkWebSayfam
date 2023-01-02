531a4fcb93a8f20200348e9c342b49bc70150e04
buraktitiz <titizburak10@gmail.com>
2023-01-03 01:57:32 +0300

    We have given an identity to our token

--- Solana/DeagleCoin/src/index.ts
+++ Solana/DeagleCoin/src/index.ts
@@ -2,6 +2,100 @@ import { initializeKeypair } from "./initializeKeypair"
 import * as web3 from "@solana/web3.js"
 import * as token from "@solana/spl-token"
 
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+  findMetadataPda,
+} from "@metaplex-foundation/js"
+import {
+  DataV2,
+  createCreateMetadataAccountV2Instruction,
+  createUpdateMetadataAccountV2Instruction,
+} from "@metaplex-foundation/mpl-token-metadata"
+import * as fs from "fs"
+
+const tokenName = "DeagleCoin"
+const description = "Welcome to rebirth of the everything."
+const symbol = "PRC"
+const decimals = 2
+const amount = 1000
+
+async function updateTokenMetadata(
+  connection: web3.Connection,
+  metaplex: Metaplex,
+  mint: web3.PublicKey,
+  user: web3.Keypair,
+  name: string,
+  symbol: string,
+  description: string
+) {
+  // file to buffer
+  const buffer = fs.readFileSync("src/images.jpeg")
+
+  // buffer to metaplex file
+  const file = toMetaplexFile(buffer, "images.jpeg")
+
+  // upload image and get image uri
+  const imageUri = await metaplex.storage().upload(file)
+  console.log("image uri:", imageUri)
+
+  // upload metadata and get metadata uri (off chain metadata)
+  const { uri } = await metaplex
+    .nfts()
+    .uploadMetadata({
+      name: name,
+      description: description,
+      image: imageUri,
+    })
+
+  console.log("metadata uri:", uri)
+
+  // get metadata account address
+  const metadataPDA = await findMetadataPda(mint)
+
+  // onchain metadata format
+  const tokenMetadata = {
+    name: name,
+    symbol: symbol,
+    uri: uri,
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  } as DataV2
+
+  // transaction to update metadata account
+  const transaction = new web3.Transaction().add(
+    createUpdateMetadataAccountV2Instruction(
+      {
+        metadata: metadataPDA,
+        updateAuthority: user.publicKey,
+      },
+      {
+        updateMetadataAccountArgsV2: {
+          data: tokenMetadata,
+          updateAuthority: user.publicKey,
+          primarySaleHappened: true,
+          isMutable: true,
+        },
+      }
+    )
+  )
+
+  // send transaction
+  const transactionSignature = await web3.sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [user]
+  )
+
+  console.log(
+    `Create Metadata Account: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  )
+}
+
 
 async function createNewMint(
   connection: web3.Connection,
@@ -119,6 +213,34 @@ async function main() {
 
   console.log("PublicKey:", user.publicKey.toBase58())
 
+
+
+
+  const MINT_ADDRESS = "Cer7JcAgVvYS4sWPxGnu6sHWQDpuye6bWojgNG3LCtBV"
+
+  // metaplex setup
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    )
+
+  // Calling the token 
+  await updateTokenMetadata(
+    connection,
+    metaplex,
+    new web3.PublicKey(MINT_ADDRESS),
+    user,
+    "DeagleCoin",
+    "DEC",
+    "Whoever holds this token is invited to my pizza party"
+  )
+
+
   const mint = await createNewMint(
     connection,
     user,           // We'll pay the fees
@@ -153,7 +275,6 @@ async function main() {
   )
 }
 
-}
 
 
 main()
